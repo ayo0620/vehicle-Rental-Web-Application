7ed48319b4a47a595b3b548c4c8e18d7e634a013
Ayomide Adewuyi <aeadewuy@Ayomides-MacBook-Pro.local>
2023-09-24 17:49:47 -0500

    Added routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
 import LandingPage from './Components/LandingPage/LandingPageDashbaord';
 
 function App() {
   return (
     <div className="App">
-      <LandingPage/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' Component={LandingPage}/>
+        {/* <Route path='/login' Component={Login}/> */}
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
